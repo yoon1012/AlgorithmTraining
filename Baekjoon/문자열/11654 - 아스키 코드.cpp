d6786714f14898b20704dc2431d246388ca350a4
Yoon Kim <ykim.imlab@gmail.com>
2019-10-22 15:28:48 +0900

    Update Baekjoon

---
+++ Baekjoon/문자열/11654 - 아스키 코드.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    char c;
+    cin >> c;
+
+    cout << static_cast<int>(c) << endl;
+    return 0;
+}
---
+++ Baekjoon/문자열/11720 - 숫자의 합.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	int n;
+	cin >> n;
+
+	string s;
+	cin >> s;
+
+	int sum = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		sum += s[i] - '0';
+	}
+
+	cout << sum << endl;
+
+	return 0;
+}
