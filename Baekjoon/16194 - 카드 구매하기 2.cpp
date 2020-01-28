574d37a7943c1986e6776916eadefaf5c6fa2334
Yoon Kim <ykim.imlab@gmail.com>
2020-01-28 19:50:19 +0900

    Update Baekjoon

---
+++ Baekjoon/16194 - 카드 구매하기 2.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int* d;
+int* p;
+
+int answer(int n)
+{
+	if (n == 0) return 0;
+	if (n == 1) return p[1];
+	if (d[n] > 0) return d[n];
+
+	int minSum = INT_MAX;
+
+	for (int i = 1; i <= n; i++)
+	{
+		int lastIndex = n - i;
+		if (lastIndex < 0) break;
+
+		int sum = answer(lastIndex) + p[i];
+
+		if (minSum > sum)
+		{
+			minSum = sum;
+		}
+	}
+
+	d[n] = minSum;
+
+	return d[n];
+} 
+
+int main(void)
+{
+	int n;
+	cin >> n;
+
+	d = new int[n + 1];
+	p = new int[n + 1];
+
+	memset(d, 0, sizeof(int) * (n + 1));
+	memset(p, 0, sizeof(int) * (n + 1));
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> p[i];
+	}
+
+	cout << answer(n) << "\n";
+
+	delete[] d;
+	delete[] p;
+
+	return 0;
+}
