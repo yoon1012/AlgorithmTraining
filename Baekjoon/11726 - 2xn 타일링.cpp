5ee6f694966418a6c68d809f6bdef039cdd25ee9
Yoon Kim <ykim.imlab@gmail.com>
2020-02-01 19:05:31 +0900

    Update Baekjoon

--- Baekjoon/11726 - 2xn 타일링.cpp
+++ Baekjoon/11726 - 2xn 타일링.cpp
@@ -6,11 +6,13 @@ int d[1001];
 
 int answer(int n)
 {
+	if (n == 0) return 0;
 	if (n == 1) return 1;
 	if (n == 2) return 2;
+
 	if (d[n] > 0) return d[n];
 
-	d[n] = answer(n - 2) + answer(n - 1);
+	d[n] = answer(n - 1) + answer(n - 2);
 	d[n] %= 10007;
 
 	return d[n];
@@ -21,6 +23,6 @@ int main(void)
 	int n;
 	cin >> n;
 
-	cout << answer(n) << "\n";
+	cout << (answer(n) % 10007) << "\n";
 	return 0;
 }
