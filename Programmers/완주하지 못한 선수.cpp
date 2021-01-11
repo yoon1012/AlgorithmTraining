9e7e4f50bf19e937a570d3f83534c7ffcc01ba06
Yoon Kim <ykim.imlab@gmail.com>
2021-01-11 19:30:43 +0900

    Add 완주하지 못한 선수
    Programmers

---
+++ Programmers/완주하지 못한 선수.cpp
@@ -0,0 +1,45 @@
+// 해시
+// Level 1
+// 2021년 01월 11일
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include <set>
+
+string solution(vector<string> participant, vector<string> completion)
+{
+	string answer = "";
+
+	multiset<string> pSet, cSet;
+
+	for (auto p : participant)
+		pSet.insert(p);
+
+	for (auto c : completion)
+		cSet.insert(c);
+
+	auto pIt = pSet.begin();
+
+	while (pIt != pSet.end())
+	{
+		auto searchResult = cSet.find(*pIt);
+
+		if (searchResult != cSet.end())
+		{
+			pSet.erase(pIt);
+			cSet.erase(searchResult);
+
+			// 완주자 multiset이 비어있는 경우
+			if (cSet.size() == 0)
+				break;
+		}
+
+		pIt++;
+	}
+
+	answer = *(pSet.begin());
+	return answer;
+}
