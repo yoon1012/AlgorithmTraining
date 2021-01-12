97582b76dbd07801b3a9a6ba5d27af96041af6ef
Yoon Kim <ykim.imlab@gmail.com>
2021-01-12 22:34:13 +0900

    Add 전화번호 목록
    Programmers

---
+++ Programmers/전화번호 목록.cpp
@@ -0,0 +1,28 @@
+// 해시
+// Level 2
+// 2021년 01월 12일
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book)
+{
+    bool answer = true;
+
+    // 접근은 맞게 했는데 최소 길이보다 긴 길이의 접두어에 대한 생각을 못했다.
+    sort(phone_book.begin(), phone_book.end());
+
+    for (int i = 0; i < phone_book.size() - 1; i++)
+    {
+        if (phone_book[i] == phone_book[i + 1].substr(0, phone_book[i].size()))
+        {
+            answer = false;
+            break;
+        }
+    }
+
+    return answer;
+}
