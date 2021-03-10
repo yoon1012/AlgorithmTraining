6797431f8276525e3374941d5689803d6c872e66
Yoon Kim <ykim.imlab@gmail.com>
2021-03-10 14:31:29 +0900

    Add K번째수
    Programmers

---
+++ Programmers/K번째수.cpp
@@ -0,0 +1,33 @@
+// 정렬
+// Level 1
+// 10-21-2019
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands)
+{
+    vector<int> answer;
+    
+    for (int i = 0 ; i < commands.size() ; i++)
+    {
+        vector<int> command = commands[i];
+        
+        int p = command[0] - 1;
+        int q = command[1] - 1;
+        int r = command[2] - 1;
+                
+        vector<int> result;
+
+        for (int j = p ; j < q + 1 ; j++)
+            result.push_back(array[j]);
+        
+        sort(result.begin(), result.end());
+        answer.push_back(result[r]);
+    }
+    
+    return answer;
+}
