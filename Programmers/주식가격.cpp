c7949908d608473efcd789e88aa9c0409d2b4fab
Yoon Kim <ykim.imlab@gmail.com>
2021-03-10 13:04:12 +0900

    Update 주식가격
    Programmers

--- Programmers/주식가격.cpp
+++ Programmers/주식가격.cpp
@@ -6,30 +6,24 @@ using namespace std;
 
 vector<int> solution(vector<int> prices)
 {
-    vector<int> answer;
-
+    vector<int> answer(prices.size());
+    stack<int> s;
+    
     for (auto i = 0; i < prices.size(); i++)
     {
-        if (i == prices.size() - 1)
+        while (!s.empty() && prices[s.top()] > prices[i])
         {
-            answer.push_back(0);
-            continue;
+            answer[s.top()] = i - s.top();
+            s.pop();
         }
 
-        stack<int> futurePrices;
+        s.push(i);
+    }
 
-        for (auto j = i + 1; j < prices.size(); j++)
-        {
-            if (prices[j] >= prices[i])
-                futurePrices.push(prices[j]);
-            else // 1초 후에 가격이 떨어진다는 것에 주의
-            {
-                futurePrices.push(prices[j]);
-                break;
-            }
-        }
-
-        answer.push_back(futurePrices.size());
+    while (!s.empty())
+    {
+        answer[s.top()] = prices.size() - s.top() - 1;
+        s.pop();
     }
 
     return answer;
