c5e665d1419ace174c3374e3a8dd98cb0928d9d2
Yoon Kim <ykim.imlab@gmail.com>
2021-03-10 14:40:19 +0900

    Update K번째수
    Programmers

--- Programmers/K번째수.cpp
+++ Programmers/K번째수.cpp
@@ -1,6 +1,6 @@
 // 정렬
 // Level 1
-// 10-21-2019
+// 03-10-2021
 
 #include <string>
 #include <vector>
@@ -11,23 +11,34 @@ using namespace std;
 vector<int> solution(vector<int> array, vector<vector<int>> commands)
 {
     vector<int> answer;
-    
-    for (int i = 0 ; i < commands.size() ; i++)
-    {
-        vector<int> command = commands[i];
-        
-        int p = command[0] - 1;
-        int q = command[1] - 1;
-        int r = command[2] - 1;
-                
-        vector<int> result;
 
-        for (int j = p ; j < q + 1 ; j++)
-            result.push_back(array[j]);
-        
-        sort(result.begin(), result.end());
-        answer.push_back(result[r]);
+    for (auto command : commands)
+    {
+        auto i = command[0] - 1;
+        auto j = command[1] - 1;
+        auto k = command[2] - 1;
+
+        vector<int> arrayCopy(j - i + 1);
+        copy(array.begin() + i, array.begin() + j + 1, arrayCopy.begin());
+        sort(arrayCopy.begin(), arrayCopy.end());
+
+        answer.push_back(arrayCopy[k]);
     }
-    
+
     return answer;
+}
+
+#include <iostream>
+
+int main(void)
+{
+    auto answer = solution({ 1, 5, 2, 6, 3, 7, 4 }, { {2, 5, 3}, {4, 4, 1}, {1, 7, 3} });
+
+    for (auto a : answer)
+    {
+        cout << a << " ";
+    }
+
+    cout << endl;
+    return 0;
 }
